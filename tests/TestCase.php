c0a7cc1677a28c1e16847d1fef971f0487ef2694
mininduhewawasam <mininduhewawasam@users.noreply.github.com>
2022-07-03 17:00:10 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,8 @@
 
 namespace Mininduhewawasam\WeightConversions\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Mininduhewawasam\WeightConversions\WeightConversionsServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
-    
 }
