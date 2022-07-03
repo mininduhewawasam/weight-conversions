791ac3ea3aa08d3dd69bb71ecb8ef0b496dbb8c9
Minindu Hewawasam <minindu@intervest.lk>
2022-07-03 22:27:31 +0530

    github actions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,9 @@
 
 namespace Mininduhewawasam\WeightConversions\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
-use Mininduhewawasam\WeightConversions\WeightConversionsServiceProvider;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-class TestCase extends Orchestra
+abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Mininduhewawasam\\WeightConversions\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            WeightConversionsServiceProvider::class,
-        ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_weight-conversions_table.php.stub';
-        $migration->up();
-        */
-    }
+    
 }
